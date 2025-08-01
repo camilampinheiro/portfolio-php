735d32e4030cbbe3b202bc08e2e317925a80628f
Camila <camilampinheiro1108@gmail.com>
2025-07-31 21:31:17 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meu Portfolio</title>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="bg-slate-900 text-white">
+
+  <header class="bg-red-600 mx-auto max-w-screen-lg px-3 py-6">
+    <div class="font-bold font-mono">
+
+    </div>
+    <p>Logo</p>
+
+  </header>
+
+</body>
+</html>
